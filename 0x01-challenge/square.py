2c1b1cdfde00206bf7c07047f9c7850c5aa696a7
Jihan <jihanahmed41@gmail.com>
2024-05-23 03:15:28 -0700

    ..

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Module that contain class square"""
 
 
-class square():
+class Square():
     """
     square class
 
@@ -23,7 +23,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
