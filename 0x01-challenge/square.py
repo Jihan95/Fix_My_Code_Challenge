9bb5b9c7dbf41dd5f47db82fec107703dac5053a
Jihan <jihanahmed41@gmail.com>
2024-05-23 03:04:40 -0700

    adding diresctory

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-"""Module that contain class square"""
 
+class square():
+    
+    width = 0
+    height = 0
 
-class Square():
-    """
-    square class
-
-    -Attributes:
-    length (int): the square length
-    """
-
-    def __init__(self, length=0):
-        """initialization method"""
-        self.length = length
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.length * self.length
+        return self.width * self.width
 
-    def perimeter_of_my_square(self):
-        """perimeter of the square"""
-        return self.length * 4
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string representation"""
-        return "{}/{}".format(self.length, self.length)
-
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = Square(12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.PermiterOfMySquare())
