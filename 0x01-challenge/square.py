fc282aa03313de20b02e76e2fe252e585a541a9a
Jihan <jihanahmed41@gmail.com>
2024-05-23 02:57:13 -0700

    updating square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,25 +7,20 @@ class Square():
     square class
 
     -Attributes:
-    width: the square width
-    height: the square height
+    length (int): the square length
     """
 
-    width = 0
-    height = 0
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self, length=0):
         """initialization method"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.length = length
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.length * 2
 
     def perimeter_of_my_square(self):
         """perimeter of the square"""
-        return (self.width * 2) + (self.height * 2)
+        return self.length * 4
 
     def __str__(self):
         """string representation"""
@@ -34,7 +29,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
